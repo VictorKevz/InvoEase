98a27d23a8bef572dfd374b39d4c1ea17c634f37
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-01-28 17:25:32 +0200

    Fix env import in i18n.js file

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,15 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const isProduction = process.env.NODE_ENV === 'production';
-const basePath = isProduction ? process.env.REACT_APP_BASE_PATH : '';
-
+const basePath = import.meta.env.VITE_BASE_PATH || "/InvoEase/";
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
     backend: {
       loadPath: `${basePath}locales/{{lng}}.json`,
